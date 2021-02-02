9f772519c875f9069ef679b6435fe887c225b41b
ReallyLiri <reallyliri@gmail.com>
2021-02-02 19:09:54 +0200

    svg setup

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,47 +1,90 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import styled from "styled-components/macro";
-import {Link} from 'react-router-dom';
 import {ReactComponent as Desktop} from '../assets/Desktop.svg'
 
 const CenteredDiv = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  
-  #Mug:hover {
-    stroke: red;
+
+  #gmail-text {
+    opacity: ${props => props.allOn || props.currentOn === "gmail" ? 1 : 0};
+  }
+
+  #gmail-sh {
+    opacity: ${props => props.allOn || props.currentOn === "gmail" ? 1 : 0};
+  }
+
+  #stackoverflow-text {
+    opacity: ${props => props.allOn || props.currentOn === "stackoverflow" ? 1 : 0};
+  }
+
+  #stackoverflow-sh {
+    opacity: ${props => props.allOn || props.currentOn === "stackoverflow" ? 1 : 0};
+  }
+
+  #github-text {
+    opacity: ${props => props.allOn || props.currentOn === "github" ? 1 : 0};
+  }
+
+  #github-sh {
+    opacity: ${props => props.allOn || props.currentOn === "github" ? 1 : 0};
+  }
+
+  #keyboard-text {
+    opacity: ${props => props.allOn || props.currentOn === "keyboard" ? 1 : 0};
+  }
+
+  #keyboard-sh {
+    opacity: ${props => props.allOn || props.currentOn === "keyboard" ? 1 : 0};
+  }
+
+  #art-text {
+    opacity: ${props => props.allOn || props.currentOn === "art" ? 1 : 0};
+  }
+
+  #art-sh {
+    opacity: ${props => props.allOn || props.currentOn === "art" ? 1 : 0};
+  }
+
+  #mobile-text {
+    opacity: ${props => props.allOn || props.currentOn === "mobile" ? 1 : 0};
+  }
+
+  #mobile-sh {
+    opacity: ${props => props.allOn || props.currentOn === "mobile" ? 1 : 0};
+  }
+
+  #book-text {
+    opacity: ${props => props.allOn || props.currentOn === "book" ? 1 : 0};
+  }
+
+  #book-sh {
+    opacity: ${props => props.allOn || props.currentOn === "book" ? 1 : 0};
   }
 `
 
-const Title = styled.div`
-  font-size: 30px;
-  font-weight: bold;
-`
+const HomePage = () => {
+    const [allOn, setAllOn] = useState(false);
+    const [currentOn, setCurrentOn] = useState(null);
 
-const StyledLink = styled(Link)`
-  font-size: 20px;
-  margin-bottom: 20px;
-`
+    useEffect(() => {
+        const addListener = (name) => {
+            const element = document.querySelector(`#${name}`);
+            element.addEventListener('mouseenter', () => setCurrentOn(name));
+            element.addEventListener('mouseover', () => setCurrentOn(null));
+            element.addEventListener('click', () => console.log("CLICK!"));
+        }
+        addListener("gmail");
+        addListener("stackoverflow");
+        addListener("github");
+        addListener("keyboard");
+        addListener("art");
+        addListener("mobile");
+        addListener("book");
+    }, [setCurrentOn])
 
-const Whitespace = styled.div`
-  height: 50px;
-`
 
-const StyledAnchor = styled.a`
-  font-size: 20px;
-  margin-bottom: 20px;
-`
-
-const ExternalLink = ({to, children}) =>
-    <StyledAnchor href={to} target="_blank" rel="noopener noreferrer">
-        {children}
-    </StyledAnchor>
-
-const HomePage = () =>
-    <CenteredDiv>
+    return <CenteredDiv allOn={allOn} currentOn={currentOn}>
         <Desktop/>
     </CenteredDiv>
+}
 
 export default HomePage;
