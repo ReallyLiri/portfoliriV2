547aa5d945d73a87ad120faf5397d5c33162765e
ReallyLiri <reallyliri@gmail.com>
2021-02-02 19:40:57 +0200

    More attempts

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,63 +1,78 @@
-import React, {useState, useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import styled from "styled-components/macro";
 import {ReactComponent as Desktop} from '../assets/Desktop.svg'
 
 const CenteredDiv = styled.div`
+  padding: 50px;
 
   #gmail-text {
     opacity: ${props => props.allOn || props.currentOn === "gmail" ? 1 : 0};
+    cursor: pointer;
   }
 
   #gmail-sh {
     opacity: ${props => props.allOn || props.currentOn === "gmail" ? 1 : 0};
+    cursor: pointer;
   }
 
   #stackoverflow-text {
     opacity: ${props => props.allOn || props.currentOn === "stackoverflow" ? 1 : 0};
+    cursor: pointer;
   }
 
   #stackoverflow-sh {
     opacity: ${props => props.allOn || props.currentOn === "stackoverflow" ? 1 : 0};
+    cursor: pointer;
   }
 
   #github-text {
     opacity: ${props => props.allOn || props.currentOn === "github" ? 1 : 0};
+    cursor: pointer;
   }
 
   #github-sh {
     opacity: ${props => props.allOn || props.currentOn === "github" ? 1 : 0};
+    cursor: pointer;
   }
 
   #keyboard-text {
     opacity: ${props => props.allOn || props.currentOn === "keyboard" ? 1 : 0};
+    cursor: pointer;
   }
 
   #keyboard-sh {
     opacity: ${props => props.allOn || props.currentOn === "keyboard" ? 1 : 0};
+    cursor: pointer;
   }
 
   #art-text {
     opacity: ${props => props.allOn || props.currentOn === "art" ? 1 : 0};
+    cursor: pointer;
   }
 
   #art-sh {
     opacity: ${props => props.allOn || props.currentOn === "art" ? 1 : 0};
+    cursor: pointer;
   }
 
   #mobile-text {
     opacity: ${props => props.allOn || props.currentOn === "mobile" ? 1 : 0};
+    cursor: pointer;
   }
 
   #mobile-sh {
     opacity: ${props => props.allOn || props.currentOn === "mobile" ? 1 : 0};
+    cursor: pointer;
   }
 
   #book-text {
     opacity: ${props => props.allOn || props.currentOn === "book" ? 1 : 0};
+    cursor: pointer;
   }
 
   #book-sh {
     opacity: ${props => props.allOn || props.currentOn === "book" ? 1 : 0};
+    cursor: pointer;
   }
 `
 
@@ -80,7 +95,6 @@ const HomePage = () => {
         addListener("mobile");
         addListener("book");
     }, [setCurrentOn])
-
 
     return <CenteredDiv allOn={allOn} currentOn={currentOn}>
         <Desktop/>
