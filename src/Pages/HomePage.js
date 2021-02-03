8c5dbc648fd199b7bc71fd0adf952f93a6665a69
ReallyLiri <reallyliri@gmail.com>
2021-02-03 19:52:06 +0200

    good outcome

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,42 +1,57 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import styled from "styled-components/macro";
-import {ReactComponent as Desktop} from '../assets/Desktop.svg'
 import navigationService from "../utils/navigationService";
+import {ReactComponent as Art} from "../assets/art.svg"
+import {ReactComponent as Book} from "../assets/book.svg"
+import {ReactComponent as Github} from "../assets/github.svg"
+import {ReactComponent as Keyboard} from "../assets/keyboard.svg"
+import {ReactComponent as Mobile} from "../assets/mobile.svg"
+import {ReactComponent as Stackoverflow} from "../assets/stackoverflow.svg"
+import {ReactComponent as Gmail} from "../assets/gmail.svg"
 
-const CenteredDiv = styled.div`
+const Container = styled.div`
   padding: 50px;
+
+  
+`
+
+const StyledImage = styled.img`
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: ${props => !props.conditional || props.isOn ? 1 : 0};
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+  pointer-events: none;
+  z-index: ${props => props.z || "unset"};
+`
+
+const StyledSvg = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: ${props => props.z || "unset"};
+  pointer-events: none;
 
   #gmail, #stackoverflow, #github, #keyboard, #art, #mobile, #book {
     cursor: pointer;
-  }
-
-  #gmail-text, #gmail-sh,
-  #stackoverflow-text, #stackoverflow-sh,
-  #github-text, #github-sh,
-  #keyboard-text, #keyboard-sh,
-  #art-text, #art-sh,
-  #mobile-text, #mobile-sh,
-  #book-text, #book-sh {
-    opacity: 0;
-    cursor: pointer;
+    pointer-events: all;
   }
 `
 
-let currentOn = null;
-
-const setCurrentOn = (name) => {
-    console.log(name)
-    if (name == null) {
-        document.querySelector(`#${currentOn}-text`).style.opacity = "0";
-        document.querySelector(`#${currentOn}-sh`).style.opacity = "0";
-        currentOn = null;
-    }
-    else {
-        document.querySelector(`#${name}-text`).style.opacity = "1";
-        document.querySelector(`#${name}-sh`).style.opacity = "1";
-        currentOn = name;
-    }
-}
+const DimensionalImage = ({src, clickable, isOn, z, ...props}) =>
+    <StyledImage
+        src={src} alt="img" draggable="false"
+        clickable={clickable}
+        z={z}
+        isOn={isOn}
+        {...props}
+    />
 
 const linkMapping = {
     "gmail": ["mailto:reallyliri@gmail.com", true],
@@ -71,7 +86,10 @@ const names = [
 
 const elementByName = {}
 
+
 const HomePage = () => {
+    const [allOn, setAllOn] = useState(false);
+    const [currentOn, setCurrentOn] = useState(null);
 
     useEffect(() => {
         const addListener = (name) => {
@@ -82,15 +100,58 @@ const HomePage = () => {
 
         for (const name of names) {
             elementByName[name] = document.querySelector(`#${name}`);
-            elementByName[`${name}-sh`] = document.querySelector(`#${name}-sh`);
-            elementByName[`${name}-text`] = document.querySelector(`#${name}-text`);
+            elementByName[name].style.cursor = "pointer";
             addListener(name);
         }
-    }, [])
 
-    return <CenteredDiv>
-        <Desktop/>
-    </CenteredDiv>
+        console.error(elementByName)
+    }, [setCurrentOn])
+
+    return <Container>
+        <DimensionalImage src={'/static/images/d1.png'} z={1}/>
+        <DimensionalImage src={'/static/images/d2.png'} z={2}/>
+        <DimensionalImage src={'/static/images/d3-keyboard-sh.png'} conditional isOn={allOn || currentOn === "keyboard"} z={3}/>
+        <DimensionalImage src={'/static/images/d4.png'} z={4}/>
+        <DimensionalImage src={'/static/images/d5-art-sh.png'} conditional isOn={allOn || currentOn === "art"} z={5}/>
+        <DimensionalImage src={'/static/images/d6.png'} z={6}/>
+        <DimensionalImage src={'/static/images/d7-github-sh.png'} conditional isOn={allOn || currentOn === "github"} z={7}/>
+        <DimensionalImage src={'/static/images/d8-stackoverflow-sh.png'} conditional isOn={allOn || currentOn === "stackoverflow"} z={8}/>
+        <DimensionalImage src={'/static/images/d9-gmail-sh.png'} conditional isOn={allOn || currentOn === "gmail"} z={9}/>
+        <DimensionalImage src={'/static/images/d11-book-sh.png'} conditional isOn={allOn || currentOn === "book"} z={11}/>
+        <DimensionalImage src={'/static/images/d12.png'} z={12}/>
+        <DimensionalImage src={'/static/images/d13-mobile-sh.png'} conditional isOn={allOn || currentOn === "mobile"} z={13}/>
+        <DimensionalImage src={'/static/images/d14.png'} z={14}/>
+        <DimensionalImage src={'/static/images/d15-book-text.png'} conditional isOn={allOn || currentOn === "book"} z={15}/>
+        <DimensionalImage src={'/static/images/d16-mobile-text.png'} conditional isOn={allOn || currentOn === "mobile"} z={16}/>
+        <DimensionalImage src={'/static/images/d17-art-text.png'} conditional isOn={allOn || currentOn === "art"} z={17}/>
+        <DimensionalImage src={'/static/images/d18-keyboard-text.png'} conditional isOn={allOn || currentOn === "keyboard"} z={18}/>
+        <DimensionalImage src={'/static/images/d19-github-text.png'} conditional isOn={allOn || currentOn === "github"} z={19}/>
+        <DimensionalImage src={'/static/images/d20-stackoverflow-text.png'} conditional isOn={allOn || currentOn === "stackoverflow"} z={20}/>
+        <DimensionalImage src={'/static/images/d21-gmail-text.png'} conditional isOn={allOn || currentOn === "gmail"} z={21}/>
+
+        <StyledSvg z={5}>
+            <Art/>
+        </StyledSvg>
+        <StyledSvg z={3}>
+            <Keyboard/>
+        </StyledSvg>
+        <StyledSvg z={7}>
+            <Github/>
+        </StyledSvg>
+        <StyledSvg z={8}>
+            <Stackoverflow/>
+        </StyledSvg>
+        <StyledSvg z={9}>
+            <Gmail/>
+        </StyledSvg>
+        <StyledSvg z={11}>
+            <Book/>
+        </StyledSvg>
+        <StyledSvg z={13}>
+            <Mobile/>
+        </StyledSvg>
+
+    </Container>
 }
 
 export default HomePage;
