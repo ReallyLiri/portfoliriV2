275c368cd0a4151de512c59e251fe4443a713e90
ReallyLiri <reallyliri@gmail.com>
2021-05-15 18:42:29 +0300

    about

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,6 +1,8 @@
 import React, {useCallback, useEffect, useState} from "react"
 import styled from "styled-components/macro";
 import Back from "../Components/Back";
+import navigationService from "../utils/navigationService";
+import FontAwesome from 'react-fontawesome'
 
 const Page = styled.div`
   width: 100vw;
@@ -43,12 +45,33 @@ const ContentBox = styled.div`
   height: ${props => props.dimension * 2}px;
 `
 
+const Paragraph = styled.p`
+  font-size: 18px;
+  margin: 32px;
+  text-align: ${props => props.align || "justify"};
+  font-weight: ${props => props.bold ? "bold" : "unset"};
+`
+
 const HeadshotImage = ({src, dimension, ...props}) =>
     <StyledImage
         src={src} alt="img" draggable="false"
         dimension={dimension}
         {...props}
     />
+
+const SocialLink = ({name, url, tip}) =>
+    <FontAwesome
+        title={tip || name}
+        className="super-crazy-colors"
+        name={name}
+        size="1x"
+        style={{cursor: "pointer", padding: "8px"}}
+        onClick={() => navigationService.openTab(url)}
+    />
+
+const Separator = styled.span`
+  font-size: 32px;
+`
 
 const imageSrc = i => `/static/images/headshots${i}.png`
 
@@ -80,7 +103,26 @@ const AboutPage = () => {
                     <HeadshotImage src={imageSrc(5)} dimension={dimension}/>
                     <HeadshotImage src={imageSrc(6)} dimension={dimension}/>
                 </VerticalStack>
-                <ContentBox dimension={dimension}>hello</ContentBox>
+                <ContentBox dimension={dimension}>
+                    <Paragraph align="center" bold>
+                        Artist | Designer | Developer
+                    </Paragraph>
+                    <Paragraph>
+                        Although my main ambition is riding on the back of a dragon towards the sunset, "Holy Diver" playing on the
+                        background, I'm compromising on being a mad (computer) scientist at day and applying the shapes of my mind
+                        to the materialized world at night.
+                    </Paragraph>
+                    <Paragraph>
+                        I believe we live in the best of all possible worlds, that we are all a part of a hive with one a collective
+                        mind and that our perception has the power to shape the very core of existence. Oh, and cats rule the world.
+                        Of course.
+                    </Paragraph>
+                    <HorizontalStack>
+                        <SocialLink name="paper-plane" url="mailto:reallyliri@gmail.com" tip="mail"/>
+                        <Separator>·</Separator>
+                        <SocialLink name="linkedin" url="http://www.linkedin.com/in/liri-sokol"/>
+                    </HorizontalStack>
+                </ContentBox>
                 <VerticalStack>
                     <HeadshotImage src={imageSrc(7)} dimension={dimension}/>
                     <HeadshotImage src={imageSrc(8)} dimension={dimension}/>
